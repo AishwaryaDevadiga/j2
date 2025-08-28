a136693a57f0de298a32328f7b56e42bdc3df9cd
AishwaryaDevadiga <159983977+AishwaryaDevadiga@users.noreply.github.com>
2025-08-28 10:56:42 +0530

    Update lab7.java

--- lab7.java
+++ lab7.java
@@ -16,43 +16,41 @@ class Student {
         age = a;
     }
 
-    // Method Overloading: display without message
+    // Method to display details
     void display() {
         System.out.println("Name: " + name + ", Age: " + age);
     }
 
-    // Method Overloading: display with custom message
+    // Overloaded method to display with message
     void display(String msg) {
-        System.out.println(msg + " -> Name: " + name + ", Age: " + age);
+        System.out.println(msg + ": Name = " + name + ", Age = " + age);
     }
 }
 
-public class lab7 {
+public class Lab7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // --- Student 1 ---
-        System.out.println("Enter details for Student 1:");
-        System.out.print("Enter name: ");
+        // Student 1 using default constructor
+        System.out.print("Enter name for Student 1: ");
         String name1 = sc.nextLine();
-        System.out.print("Enter age: ");
+        System.out.print("Enter age for Student 1: ");
         int age1 = sc.nextInt();
         sc.nextLine(); // consume newline
 
-        Student s1 = new Student(); // using default constructor
+        Student s1 = new Student();
         s1.name = name1;
         s1.age = age1;
 
-        // --- Student 2 ---
-        System.out.println("\nEnter details for Student 2:");
-        System.out.print("Enter name: ");
+        // Student 2 using parameterized constructor
+        System.out.print("Enter name for Student 2: ");
         String name2 = sc.nextLine();
-        System.out.print("Enter age: ");
+        System.out.print("Enter age for Student 2: ");
         int age2 = sc.nextInt();
 
-        Student s2 = new Student(name2, age2); // using parameterized constructor
+        Student s2 = new Student(name2, age2);
 
-        // --- Display Students ---
+        // Display both students
         System.out.println("\n--- Student Details ---");
         s1.display("Student 1");
         s2.display("Student 2");
